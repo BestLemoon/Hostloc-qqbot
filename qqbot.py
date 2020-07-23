df64905d808e415560cdf59107960c373d203f97
Lemooonade <53417050+Lemooonade@users.noreply.github.com>
2020-07-23 13:38:23 +0800

    update

--- qqbot.py
+++ qqbot.py
@@ -2,17 +2,18 @@ from spider import *
 import requests
 import json
 import time
-api = 'https://sc.ftqq.com/{YOUR_KEY}.send'
+api = 'https://sc.ftqq.com/SCU99459T0f4dcd6a01a0a9e5696490e07a05eacc5ecdd6b648de6.send'
 #server酱的api接口，将{YOUR_KEY}替换成自己的，不需要的可以删除所有带api的语句，仅用于程序出错的通知
+
 def send_msg(msg):
     data = {
-        'group_id': '',#qq群号(int) 可替换成个人'user_id':''
+        'user_id': '',#qq号(int) 可替换成qq群'group_id':''
         'message': msg,
         'auto_escape': False
     }
-    r = requests.post('http://127.0.0.1:5700/send_group_msg', data=data)#个人：将send_group_msg替换为/send_private_msg
+    r = requests.post('http://127.0.0.1:5700/send_private_msg', data=data)#qq群：将send_private_msg替换为send_group_msg
     ret = json.loads(r.text)
-    if ret['retcode'] != 0:#qq发送状态
+    if ret['retcode'] != 0:
         data = {
             "text": 'locbot炸了',
             "desp": '发送失败！'
@@ -31,12 +32,12 @@ if __name__=="__main__":
             }
             requests.post(api, data=data)
         print('抓取成功')
-        for i in range(len(urls)):
+        for i in range(10):
             if urls[i] not in raw_urls:
                 raw_urls.append(urls[i])
-                raw_titles.append(titles[i])
+                raw_titles.append(titles[i])   
                 send_msg(titles[i]+'\n'+urls[i])
-        time.sleep(5)
+        time.sleep(30)
 
 
 
--- spider.py
+++ spider.py
@@ -1,25 +1,40 @@
 import requests
 from lxml import etree
 
-
+def login():
+    session=requests.session()
+    header = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
+    }
+    url = "https://www.hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1"
+    data = {
+        'fastloginfield': 'username',
+        'username': 'Lemoon',
+        'password': 'Z454599012jude.',
+        'quickforward': 'yes',
+        'handlekey': 'ls'
+    }
+    r = session.post(url, data, header)
+    return r.cookies
+    
 def get_urls():
     global response
     url = 'https://www.hostloc.com/forum.php?mod=forumdisplay&fid=45&filter=author&orderby=dateline'
-    api = 'https://sc.ftqq.com/{YOUR_KEY}.send'
+    api = 'https://sc.ftqq.com/SCU99459T0f4dcd6a01a0a9e5696490e07a05eacc5ecdd6b648de6.send'
     #server酱的api接口，将{YOUR_KEY}替换成自己的，不需要的可以删除所有带api的语句，仅用于程序出错的通知
     headers = {
         'referer': 'https://www.hostloc.com/forum.php?mod=forumdisplay&fid=45&filter=author&orderby=dateline',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-        'cookie': ''
-    }#cookie为自己摘取的，应该不具有兼容性，若urls和titles没有内容，是触发了反爬机制，后期考虑再改进
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+        }#新版去除了麻烦的cookie，直接登录即可
     try:
         response = requests.get(url, headers=headers)
+        print(response.status_code)
     except Exception as e:
         data = {
             "text": 'locbot炸了',
             "desp": e
         }
-        requests.post(api, data=data)
+        requests.post(api, data=data,cookie=login())
     urls = []
     #print(response.text)
     html = etree.HTML(response.text)
